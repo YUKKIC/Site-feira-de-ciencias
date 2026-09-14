336046ae9266d53d182d284374e0820a68792901
YUKKIC <bastosgabriel392@gmail.com>
2026-09-14 09:46:08 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const links = document.querySelectorAll('.nav-links a');
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelector('.nav-links')?.classList.remove('open');
-  });
-});
